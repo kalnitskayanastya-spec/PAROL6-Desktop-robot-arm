7ef5a41551276f6f3b51dbd343fc30b22d3bad34
Анастасия Кальницккя <anastasiakalnickka@Mac.lan>
2026-06-05 18:30:24 +0300

    Add Octopus F446 USB CDC test firmware

---
+++ PAROL6 control board main software/src/octopus_usb_test.cpp
@@ -0,0 +1,61 @@
+#include <Arduino.h>
+
+extern "C" void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {};
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 6;
+    RCC_OscInitStruct.PLL.PLLN = 168;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        while (1) {}
+    }
+
+    RCC_ClkInitStruct.ClockType =
+        RCC_CLOCKTYPE_HCLK |
+        RCC_CLOCKTYPE_SYSCLK |
+        RCC_CLOCKTYPE_PCLK1 |
+        RCC_CLOCKTYPE_PCLK2;
+
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+        while (1) {}
+    }
+}
+
+void setup()
+{
+    SerialUSB.begin(115200);
+    delay(1000);
+
+    SerialUSB.println();
+    SerialUSB.println("================================");
+    SerialUSB.println("Octopus Pro F446 USB test alive");
+    SerialUSB.println("HSE: 12 MHz");
+    SerialUSB.println("SYSCLK: 168 MHz");
+    SerialUSB.println("USB clock: 48 MHz");
+    SerialUSB.println("Flash origin test: 0x08000000");
+    SerialUSB.println("No CAN, no TMC, no PAROL6 runtime");
+    SerialUSB.println("================================");
+}
+
+void loop()
+{
+    SerialUSB.print("millis = ");
+    SerialUSB.println(millis());
+    delay(1000);
+}
